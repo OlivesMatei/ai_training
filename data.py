fc5a3e7056a6198a1c3ac3059d1682aff0550ba4
OliveMatei <smatei@kabarak.ac.ke>
2024-07-03 20:21:35 +0300

    models ne

--- data.py
+++ data.py
@@ -33,7 +33,7 @@ y = features[['diabetes', 'heart_disease', 'alzheimer']]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Scale features
+# the Scale features
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
